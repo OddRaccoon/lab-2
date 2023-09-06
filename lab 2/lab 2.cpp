a2197192c7a5d52f30ac092338a55c3be1fc7f54
OddRaccoon8 <116515602+OddRaccoon8@users.noreply.github.com>
2023-09-06 09:38:03 -0500

    Add project files.

---
+++ lab 2/lab 2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float A;
+    float B;
+    float X;
+
+    cout << "Hello, my name is Renee and I'm going to solve the equation: Ax + B = 0 for x ";
+
+    cout << "Please enter a value for A: -3 ";
+    cout << "Please enter a value for B: 5 ";
+    cout << "Solving -3x+5=0 for x...";
+    cin >> A;
+    cout << "A=" << A << endl;
+    B = (A * 4) + 1;
+    cout << "And B=" << B << endl;
+
+    cout << 0 - 5 / -3 = X;
+
+}
